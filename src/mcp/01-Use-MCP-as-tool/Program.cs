0baf9e5600b8b409a19953dac72915ec8d4789e4
Adrien Clerbois <adrien.clerbois@outlook.com>
2025-11-05 22:47:52 +0100

    feat: Initialize MCP tool with Azure OpenAI client and implement issue summarization

---
+++ src/mcp/01-Use-MCP-as-tool/Program.cs
@@ -0,0 +1,38 @@
+﻿// Set up the Azure OpenAI client
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
+using System.ComponentModel;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Workflows;
+using Microsoft.Extensions.AI;
+using System;
+using OpenAI;
+
+
+await using var mcpClient = await McpClient.CreateAsync(new StdioClientTransport(new()
+{
+    Name = "MCPServer",
+    Command = "npx",
+    Arguments = ["-y", "--verbose", "@modelcontextprotocol/server-github"],
+}));
+
+var mcpTools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+
+
+
+
+var agent = new AzureOpenAIClient(
+    new Uri("https://devday-2025-maf.openai.azure.com/"),
+    new AzureCliCredential())
+        .GetChatClient("gpt-4o")
+ .CreateAIAgent(
+    instructions: "You answer questions related to GitHub repositories only.",
+    tools: [.. mcpTools.Cast<AITool>()]);
+
+System.Console.WriteLine("Q: Summarize the last four issues to the microsoft/agent-framework repository authored by AClerbois?");
+// Invoke the agent and output the text result
+Console.WriteLine(await agent.RunAsync("Summarize the last four issues to the microsoft/agent-framework repository authored by AClerbois?"));
